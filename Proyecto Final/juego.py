7897efd212de490c38765306811b714b0bcdbb86
Jeefry_C <je.cardona08@ciaf.edu.co>
2025-05-17 21:47:53 -0500

    Agrega juego base con ventana en blanco

---
+++ Proyecto Final/juego.py
@@ -0,0 +1,27 @@
+import pygame
+import sys 
+
+# Inicializar pygame
+if __name__ == '__main__':
+    pygame.init()
+
+    # Crear una ventana
+    ventana = pygame.display.set_mode((1000, 600))
+    pygame.display.set_caption("Battle City")
+
+
+    # Colores
+    BLANCO = (255, 255, 255)
+
+    # Bucle principal
+    while True:
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+
+        # Llenar la pantalla con un color
+        ventana.fill(BLANCO)
+
+        # Actualizar la pantalla
+        pygame.display.update()
